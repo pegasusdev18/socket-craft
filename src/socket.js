c4d72165b7053626fc96d3d4a433912a8d46b820
Mohammad Mahdi <pegasusdev18@gmail.com>
2026-07-09 09:29:22 +0200

    Refactor socket handling with RoomEmitter and BroadcastEmitter

--- src/socket.js
+++ src/socket.js
@@ -1,25 +1,93 @@
 import { randomUUID } from 'node:crypto'
 
+export class RoomEmitter {
+  constructor(socket, room) {
+    this.socket = socket
+    this.room = room
+  }
+
+  emit(event, data) {
+    const socket = this.socket
+    const store = socket.store
+    const room = this.room
+    const selfId = socket.id
+    const members = store.getRoomMembers(room)
+    if (!members) return
+    const payload = JSON.stringify({ event, data })
+    for (const memberId of members) {
+      if (memberId === selfId) continue
+      const client = store.getClient(memberId)
+      if (client) client.sendRaw(payload)
+    }
+  }
+}
+
+export class BroadcastEmitter {
+  constructor(socket) {
+    this.socket = socket
+  }
+
+  emit(event, data) {
+    const socket = this.socket
+    const store = socket.store
+    const namespace = socket.namespace
+    const selfId = socket.id
+    const payload = JSON.stringify({ event, data })
+    for (const client of store.getAllClients()) {
+      if (client.id === selfId) continue
+      if (client.namespace !== namespace) continue
+      client.sendRaw(payload)
+    }
+  }
+}
+
 export class SocketCraftSocket {
   constructor(rawSocket, options) {
     this.id = randomUUID()
     this.raw = rawSocket
     this.namespace = options.namespace
     this.store = options.store
+    this.handlers = options.handlers || null
+    this.maxBackpressure = options.maxBackpressure || 1048576
     this.data = {}
     this.rooms = new Set()
+    this.params = {}
     this.query = {}
     this.request = null
     this.isAlive = true
+    this._broadcastEmitter = new BroadcastEmitter(this)
+  }
+
+  _isBackpressured() {
+    if (this.raw.bufferedAmount <= this.maxBackpressure) return false
+    if (this.handlers && typeof this.handlers.onBackpressure === 'function') {
+      this.handlers.onBackpressure(this)
+    } else {
+      this.raw.close(1009, 'Buffer Overflow')
+    }
+    return true
   }
 
   send(event, data) {
-    if (this.raw.readyState !== this.raw.OPEN) return
-    this.raw.send(JSON.stringify({ event, data }))
+    return this.sendRaw(JSON.stringify({ event, data }))
+  }
+
+  sendRaw(rawPayload) {
+    if (this.raw.readyState !== this.raw.OPEN) return false
+    if (this._isBackpressured()) return false
+    this.raw.send(rawPayload)
+    return true
+  }
+
+  sendBinary(buffer) {
+    if (this.raw.readyState !== this.raw.OPEN) return false
+    if (this._isBackpressured()) return false
+    this.raw.send(buffer, { binary: true })
+    return true
   }
 
   emit(event, data) {
-    this.send(event, data)
+    return this.send(event, data)
   }
 
   join(room) {
@@ -33,34 +101,11 @@ export class SocketCraftSocket {
   }
 
   to(room) {
-    const store = this.store
-    const selfId = this.id
-    return {
-      emit(event, data) {
-        const members = store.getRoomMembers(room)
-        if (!members) return
-        for (const memberId of members) {
-          if (memberId === selfId) continue
-          const client = store.getClient(memberId)
-          if (client) client.send(event, data)
-        }
-      }
-    }
+    return new RoomEmitter(this, room)
   }
 
   get broadcast() {
-    const store = this.store
-    const namespace = this.namespace
-    const selfId = this.id
-    return {
-      emit(event, data) {
-        for (const client of store.getAllClients()) {
-          if (client.id === selfId) continue
-          if (client.namespace !== namespace) continue
-          client.send(event, data)
-        }
-      }
-    }
+    return this._broadcastEmitter
   }
 
   disconnect(code = 1000, reason = 'Normal closure') {
